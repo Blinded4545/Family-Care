693d28f0fa651dcd0533e36409a3f47b299908db
Blinded4545 <bluemistery82@gmail.com>
2024-08-06 13:21:13 -0500

    Added responsiveness to catalogo page for 1920x1080 screen resolution

--- src/components/cardComponent.jsx
+++ src/components/cardComponent.jsx
@@ -8,10 +8,10 @@ const CardComponent = ({title, price, description, url}) => {
             <Card id="cardComp" className="d-flex align-items-center shadow-sm" style={{"min-width": "min-content"}}>
                 <Card.Img variant="top" src={url} className="w-75"/>
                 <Card.Body>
-                    <Card.Title className="fs-4">{title}</Card.Title>
-                    <Card.Subtitle><p className="lh-1 fw-normal text-break">{description}</p></Card.Subtitle>
-                    <Card.Text className="fs-5 fw-bold">${price}</Card.Text>
-                    <Button variant="outline-primary w-100" onClick={() => console.log("Success")}>
+                    <Card.Title className="fs-4"><p id="titText">{title}</p></Card.Title>
+                    <Card.Subtitle><p className="lh-1 fw-normal text-break" id="descriptionText">{description}</p></Card.Subtitle>
+                    <Card.Text className="fs-5 fw-bold" id="priceText">${price}</Card.Text>
+                    <Button variant="outline-primary w-100" id="buttonText" onClick={() => console.log("Success")}>
                         Ver mas
                     </Button>
                 </Card.Body>
--- src/pages/Catalogo.jsx
+++ src/pages/Catalogo.jsx
@@ -19,20 +19,19 @@ const Catalogo = ()=>{
                 <div className="mx-5 my-4 p-3 fs-4">
                     <div>
                         <Form className="d-flex flex-column gap-4">
-                            <Form.Group controlId="form-group-id" className="">
-                                <Form.Label className="fs-5">Buscar</Form.Label>
+                            <Form.Group>
+                                <Form.Label className="fs-5" id="formTitle">Buscar</Form.Label>
                                 <div className="d-flex flex-row">  
-                                    <Form.Control type="text" placeholder="Producto" onChange={(e)=>{
+                                    <Form.Control id="formInput" type="text" placeholder="Producto" onChange={(e)=>{
                                     setCurrSearch(e.target.value)
-                                    console.log(currSearch);
                                 }}/>
-                                    <DropdownButton title={currFilter===""?"Tipo de busqueda":currFilter==="1"?"Titulo":"Categoria"} id="dropdown-button-example">
-                                        <Dropdown.Header>Filtros</Dropdown.Header>
+                                    <DropdownButton title={currFilter===""?"Tipo de busqueda":currFilter==="1"?"Titulo":"Categoria"} id="formDropdown">
+                                        <Dropdown.Header className="dropdownText">Filtros</Dropdown.Header>
                                         <Dropdown.Divider />
-                                        <Dropdown.Item eventKey="1" onClick={(e)=>{
+                                        <Dropdown.Item className="dropdownText" eventKey="1" onClick={(e)=>{
                                             setCurrFilter("1")
                                         }}>Titulo</Dropdown.Item>
-                                        <Dropdown.Item eventKey="2" onClick={(e)=>{
+                                        <Dropdown.Item className="dropdownText" eventKey="2" onClick={(e)=>{
                                             setCurrFilter("2")
                                         }}>Categoria</Dropdown.Item>
                                     </DropdownButton>
@@ -44,21 +43,21 @@ const Catalogo = ()=>{
                 <div className="border border-2 border-secondary rounded-3 d-flex mb-3 shadow-lg">
                     <div className="p-5 d-flex flex-wrap flex-row w-100 justify-content-center align-items-center gap-4">
                         {
-                            categories.map((e)=>{
+                            categories.map((e, idx)=>{
                                 if((currFilter==="2" || currFilter==="") && e.toLowerCase().includes(currSearch)){
                                     return(
                                         <>
-                                            <h3>{e}</h3>
-                                            <div className="container-fluid justify-content-center align-items-center gap-4">
+                                            <h3 className="categoryTitle">{e}</h3>
+                                            <div key={idx} className="container-fluid justify-content-center align-items-center gap-4">
                                                 <div className="row row-cols-4 gap-4 justify-content-center">
                                                     {
-                                                        productList.map((e2)=>{
+                                                        productList.map((e2, idy)=>{
                                                             if(e2.category!==e){
                                                                 return
                                                             }
                                                             else{
                                                                 return (
-                                                                    <CardComponent title={e2.title} price={e2.price} description={e2.description} url={e2.url}/>
+                                                                    <CardComponent key={idy} title={e2.title} price={e2.price} description={e2.description} url={e2.url}/>
                                                                 )
                                                             }
                                                         })
