dceb848c5da6071a7f55dc9b1b58546764f300a4
Blinded4545 <bluemistery82@gmail.com>
2024-08-07 18:08:55 -0500

    Added photos to the catalogo page

--- src/components/lists.jsx
+++ src/components/lists.jsx
@@ -1,66 +1,79 @@
+
+import so1 from "../assets/solido1.png"
+import so2 from "../assets/solido2.png"
+import so3 from "../assets/solido3.png"
+
+import li1 from "../assets/liquido1.webp"
+import li2 from "../assets/liquido22.png"
+import li3 from "../assets/liquido3.webp"
+
+import ma1 from "../assets/manos1.png"
+import ma2 from "../assets/manos22.png"
+import ma3 from "../assets/manos3.png"
+
 export const productList = [
     {
         title: "Jabon manos c",
         category: "Para manos",
         price: 75,
         description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit, minima. Veritatis, ad. A, recusandae similique provident natus totam animi expedita dicta veritatis voluptatem? Culpa quibusdam alias esse, magni veniam quod.",
-        url: "https://picsum.photos/seed/1/200/200"
+        url: ma1
     },
     {
         title: "Jabon liquido b",
         category: "Corporal liquido",
         price: 75,
         description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit, minima. Veritatis, ad. A, recusandae similique provident natus totam animi expedita dicta veritatis voluptatem? Culpa quibusdam alias esse, magni veniam quod.",
-        url: "https://picsum.photos/seed/2/200/200"
+        url: li1
     },
     {
         title: "Jabon solido a",
         category: "Corporal solido",
         price: 75,
         description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit, minima. Veritatis, ad. A, recusandae similique provident natus totam animi expedita dicta veritatis voluptatem? Culpa quibusdam alias esse, magni veniam quod.",
-        url: "https://picsum.photos/seed/3/200/200"
+        url: so1
     },
     {
         title: "Jabon manos cc",
         category: "Para manos",
         price: 75,
         description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit, minima. Veritatis, ad. A, recusandae similique provident natus totam animi expedita dicta veritatis voluptatem? Culpa quibusdam alias esse, magni veniam quod.",
-        url: "https://picsum.photos/seed/4/200/200"
+        url: ma2
     },
     {
         title: "Jabon liquido bb",
         category: "Corporal liquido",
         price: 75,
         description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit, minima. Veritatis, ad. A, recusandae similique provident natus totam animi expedita dicta veritatis voluptatem? Culpa quibusdam alias esse, magni veniam quod.",
-        url: "https://picsum.photos/seed/5/200/200"
+        url: li2
     },
     {
         title: "Jabon solido aa",
         category: "Corporal solido",
         price: 75,
         description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit, minima. Veritatis, ad. A, recusandae similique provident natus totam animi expedita dicta veritatis voluptatem? Culpa quibusdam alias esse, magni veniam quod.",
-        url: "https://picsum.photos/seed/6/200/200"
+        url: so2
     },
     {
         title: "Jabon manos ccc",
         category: "Para manos",
         price: 75,
         description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit, minima. Veritatis, ad. A, recusandae similique provident natus totam animi expedita dicta veritatis voluptatem? Culpa quibusdam alias esse, magni veniam quod.",
-        url: "https://picsum.photos/seed/7/200/200"
+        url: ma3
     },
     {
         title: "Jabon liquido bbb",
         category: "Corporal liquido",
         price: 75,
         description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit, minima. Veritatis, ad. A, recusandae similique provident natus totam animi expedita dicta veritatis voluptatem? Culpa quibusdam alias esse, magni veniam quod.",
-        url: "https://picsum.photos/seed/8/200/200"
+        url: li3
     },
     {
         title: "Jabon solido aaa",
         category: "Corporal solido",
         price: 75,
         description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit, minima. Veritatis, ad. A, recusandae similique provident natus totam animi expedita dicta veritatis voluptatem? Culpa quibusdam alias esse, magni veniam quod.",
-        url: "https://picsum.photos/seed/9/200/200"
+        url: so3
     }
 ]
 export const categories = [
@@ -68,23 +81,3 @@ export const categories = [
     "Corporal liquido",
     "Para manos"
 ]
-
-const a = ()=>{
-    if(currSearch===""){ 
-        if(e2.category!==e){
-            return
-        }
-        else{
-            return (
-                <CardComponent title={e2.title} price={e2.price} description={e2.description} group={e2.category}/>
-            )
-        }
-    }
-    else{
-        if(e2.title.toLowerCase().includes(currSearch.toLowerCase()) && e2.category===e){
-            return (
-                <CardComponent title={e2.title} price={e2.price} description={e2.description} group={e2.category}/>
-            )
-        }
-    }
-}
