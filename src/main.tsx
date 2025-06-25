fc41f0ac81f7521c84eaaab38b1d75dcdc66b74f
Blinded454 <bluemistery82@gmail.com>
2025-06-25 01:12:42 -0500

    Think I fixed it

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,30 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+import { MainView } from './views/MainView.tsx'
+import { JabonManos } from './views/JabonManos.tsx'
+import { JabonCorporal } from './views/JabonCorporal.tsx'
+import { JabonSolido } from './views/JabonSolido.tsx'
+import { Detergente } from './views/Detergente.tsx'
+import { Limpiadores } from './views/Limpiadores.tsx'
+import { Contacto } from './views/Contacto.tsx'
+
+
 import './index.css'
-import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' index element={<MainView/>} ></Route>
+        <Route path='/jabon-manos' element={<JabonManos/>}></Route>
+        <Route path='/jabon-liquido' element={<JabonCorporal/>}></Route>
+        <Route path='/jabon-solido' element={<JabonSolido/>}></Route>
+        <Route path='/detergente' element={<Detergente/>}></Route>
+        <Route path='/desinfectantes' element={<Limpiadores/>}></Route>
+        <Route path='/contacto' element={<Contacto/>}></Route>
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
